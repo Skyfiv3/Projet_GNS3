53f43298881a96fbe385188075d9c495ccd7290d
Skyfiv3 <baptrgtt@gmail.com>
2024-01-10 10:43:05 +0100

    Fonction adressage début

--- DnD/main.py
+++ DnD/main.py
@@ -39,10 +39,43 @@ chemin_data = os.path.join(os.path.dirname(__file__),'..','data','data.json')
 
 with open(chemin_data,"r") as data :
     intentions = json.load(data)
-print(intentions)
 
 def constante(router) :
     
     return "!\n!\n!\n!\n!\n!\n!\n!\n\n!\n! Last configuration change at 14:16:26 UTC Wed Dec 20 2023\n!\nversion 15.2\nservice timestamps debug datetime msec\nservice timestamps log datetime msec\n!\nhostname "+router+"\n!\nboot-start-marker\nboot-end-marker\n!\n!\n!\nno aaa new-model\nno ip icmp rate-limit unreachable\nip cef\n!\n!\n!\n!\n!\n!\nno ip domain lookup\nipv6 unicast-routing\nipv6 cef\n!\n!\nmultilink bundle-name authenticated\n!\n!\n!\n!\n!\n!\n!\n!\n!\nip tcp synwait-time 5\n! \n!\n!\n!\n!\n!\n!\n!\n!\n!\n!\n!"
 
+def adressage(data):
+    for AS in data["AS"]:
+        adresse=data["AS"][AS]["plage_IP"]["interfaces_physique"]
+        nombre_liens=len(data["AS"][AS]["liens"])
     
+    
+
+
+        # Séparer les éléments en fonction des ":"
+        elements = adresse.split(':')
+
+        # Remplacer "/64" par une chaîne vide
+        elements[-1] = elements[-1].replace('/64', '')
+
+        # Convertir les éléments en entiers
+        elements = [int(e, 16) if e else 0 for e in elements]
+
+        # Si le nombre d'éléments est inférieur à 4, remplir avec des 0
+        while len(elements) < 4:
+            elements.append(0)
+
+        resultats = []
+
+        # Créer une plage d'adresses pour chaque lien
+        for i in range(nombre_liens):
+            adresse_lien = elements.copy()  # Créer une copie pour chaque itération
+            adresse_lien[-1] += i  # Incrémenter le dernier élément pour chaque lien
+            adresse_lien_str = ':'.join([hex(e)[2:] for e in adresse_lien]) + '::/64'
+
+            data["AS"][AS]["liens"][i].append(adresse_lien_str)
+
+        
+
+adressage(intentions)
+print(intentions)
